2da451c0fd747096d9b82e59ca2799ebabf202a7
LavenderPixl <annajacobsen1999@hotmail.com>
2024-10-24 10:46:17 +0200

    Projects View/Page finished.

--- src/components/ProjectBox.vue
+++ src/components/ProjectBox.vue
@@ -3,24 +3,84 @@ export default {
   props: {
     name: String,
     description: String,
-    url: String,
-    updated_at: String
+    language: String,
+    updated_at: String,
+    html_url: String,
   },
+  methods: {
+    redirect() {
+      window.location.href= this.html_url
+    }
+  }
 }
 </script>
 
 <template>
-  <div class="container" @click="$emit('clicked {{this.title}}')">
-    <p>{{this.name }}</p>
-    <p>{{this.description}}</p>
-    <p>{{this.url}}</p>
-    <p>{{this.updated_at}}</p>
-  </div>
+  <a @click="redirect()">
+    <div class="container">
+      <div id="top">
+        <p id="name">{{ this.name }}</p>
+        <p id="desc">{{ this.description }}</p>
+      </div>
+      <div id="bottom">
+        <p id="language">{{ this.language }}</p>
+        <p id="date">{{ this.updated_at }}</p>
+      </div>
+    </div>
+  </a>
 </template>
 
 <style scoped>
 .container {
-  background: red;
-  height: 10vh;
+  background: var(--color-background);
+  border: var(--border-color) 2px solid;
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  padding-left: 1vw;
+  padding-right: 1vw;
+}
+
+#top {
+  display: flex;
+  flex-direction: column;
+}
+
+#name {
+  margin-top: 1vw;
+  margin-bottom: 0;
+  font-size: 1.2vw;
+  text-decoration: underline;
+}
+
+#desc {
+  font-size: 1vw;
+  height: 3vw;
+}
+
+#bottom {
+  //height: 3vw;
+  display: flex;
+}
+
+#bottom p {
+  width: 100%;
+  margin: 0 0 1vw 0;
+  //margin-bottom: 1vw;
+}
+
+#language {
+  text-align: left;
+  font-size: 1vw;
+}
+
+#date {
+  text-align: right;
+  font-size: 1vw;
+}
+
+a {
+  text-decoration-line: none;
+  color: var(--color-text);
 }
 </style>
--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,56 +1,161 @@
 <script>
 import ProjectBox from '@/components/ProjectBox.vue'
+import axios from 'axios'
 
 export default {
   components: { ProjectBox },
-  mounted() {
-    // this.getRepos()
-  },
   data() {
     return {
       currentPage: 1,
+      // displaying: 9,
       repos: [],
       repo: {
         name: '',
         description: '',
-        url: '',
-        updated_at: ''
-      }
+        language: '',
+        updated_at: '',
+        html_url: '',
+      },
+      displaying: [],
     }
   },
+  async mounted() {
+    await this.getRepos()
+  },
   methods: {
-    // async getRepos() {
-    //   const res = await fetch('https://api.github.com/users/LavenderPixl/repos')
-    //   const projects = await res.json()
-    //   for (let project of projects) {
-    //     this.repos.push(project)
-    //   }
-    // },
-    // pager(btnClicked) {
-    //   let page = btnClicked
-    //
-    //    btnClicked
-    //   if page
-    // }
-  }
+    async getRepos() {
+      await axios
+        .get('https://api.github.com/users/LavenderPixl/repos', {
+          headers: {
+            'User-Agent': 'LavenderPixl',
+            Authorization: `Bearer ${import.meta.env.VITE_GTOKEN}`,
+          },
+        })
+        .then(res => {
+          // const projects = res.json
+          for (let project of res.data) {
+            this.repos.push(project)
+          }
+
+          this.repos.sort(
+            (a, b) => Date.parse(a.updated_at) - Date.parse(b.updated_at),
+          )
+          this.repos.reverse()
+
+          this.displaying = this.repos.slice(0, 9)
+        })
+    },
+
+    pager(btnClicked) {
+      if (btnClicked === '-') {
+        if (this.currentPage > 1) {
+          this.displaying.pop()
+          this.currentPage--
+        } else return
+      }
+      if (btnClicked === '+' && this.currentPage < this.repos.length / 9) {
+        this.displaying.pop()
+        this.currentPage++
+      }
+
+      let start = this.getStart()
+      this.displaying = this.repos.slice(start, start + 9)
+    },
+    getStart() {
+      let start = (this.currentPage - 1) * 9
+      Math.round(start)
+      return start
+    },
+  },
 }
 </script>
 
 <template>
-  <div v-for="(repo, index) in repos" :key="index">
-    <ProjectBox
-      :name="repo.name"
-      :description="repo.description"
-      :url="repo.url"
-      :updated_at="repo.updated_at"
-    ></ProjectBox>
-  </div>
-
-  <div class="pager">
-<!--    <button @click="pager(0)"></button>
-    <button @click="pager(1)"></button>-->
+  <div id="wrapper">
+    <!--    <SideBar />-->
+    <div id="topBar">
+      <RouterLink to="/" id="back"> &lt; Back to Home</RouterLink>
+      <p id="topLine">Click a project to view it on Github.</p>
+    </div>
+    <div class="repos">
+      <ProjectBox
+        v-for="(repo, index) in displaying"
+        :key="index"
+        :name="repo.name"
+        :description="repo.description"
+        :language="repo.language"
+        :updated_at="repo.updated_at"
+        :html_url="repo.html_url"
+      ></ProjectBox>
+    </div>
+    <div id="pager">
+      <button @click="pager('-')" id="btn">&lt;</button>
+      <p id="pageNumber">{{ this.currentPage }}</p>
+      <button @click="pager('+')" id="btn">&gt;</button>
+    </div>
   </div>
 </template>
 
 <style>
+#wrapper {
+  width: 100%;
+  display: flex;
+  overflow: scroll;
+  flex-direction: column;
+}
+
+#topBar {
+  width: 100%;
+  margin-top: 2vw;
+  margin-bottom: 1vw;
+  font-size: 1vw;
+  display: flex;
+  flex-direction: row;
+}
+
+#topLine {
+  width: 100%;
+  margin-top: 0;
+}
+
+#back {
+  width: 70%;
+  color: var(--color-text);
+  margin-left: 2%;
+  text-decoration: underline;
+}
+.repos {
+  justify-content: center;
+  display: grid;
+  grid-template-columns: 25vw 25vw 25vw;
+  column-gap: 4vw;
+  grid-template-rows: 10vw 10vw 10vw;
+  row-gap: 4vw;
+}
+
+#pager {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 2vw;
+}
+
+#btn {
+  height: 1.5vw;
+  width: 3vw;
+  font-size: 1vw;
+  border: var(--border-color) 2px solid;
+  background-color: #a3b18a;
+  cursor: pointer;
+}
+
+#pageNumber {
+  margin-left: 1vw;
+  margin-right: 0.4vw;
+  margin-top: 0;
+  font-size: 1vw;
+  width: 1vw;
+  text-decoration: underline;
+  //margin: 0;
+}
 </style>
