a6c46e8878d9c39bf079bc93a1e8d6cc10426a89
LavenderPixl <annajacobsen1999@hotmail.com>
2024-10-24 10:45:49 +0200

    Cleanup!

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,9 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template class="app-container">
   <header>
-<!--    <nav>-->
-<!--      <RouterLink to="/">Home</RouterLink>-->
-<!--      <RouterLink to="/projects">Projects</RouterLink>-->
-<!--    </nav>-->
-    <!--    <img-->
-<!--      alt="Vue logo"-->
-<!--      class="logo"-->
-<!--      src="@/assets/logo.svg"-->
-<!--      width="125"-->
-<!--      height="125"-->
-<!--    />-->
   </header>
 
   <RouterView />
